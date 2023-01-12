947a4935c017d2debece891185b1b232cd1913d7
Mundiaem <machariamundia@gmail.com>
2023-01-12 22:35:32 +0300

    initial commit

---
+++ src/test/java/com/loan/repaymentapi/RepaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan.repaymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepaymentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
